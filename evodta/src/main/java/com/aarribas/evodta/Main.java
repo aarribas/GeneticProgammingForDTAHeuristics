26810995923005afd2150076af677c09192abc11
aarribas <andres.arribas@gmail.com>
2013-04-11 14:48:28 +0200

    First test using original ecj example from Rinde.
    Copied original param files 
    Created package with evaluator and functions EvoDTA classes.

---
+++ evodta/src/main/java/com/aarribas/evodta/Main.java
@@ -0,0 +1,17 @@
+package com.aarribas.evodta;
+
+import ec.Evolve;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Evolve.main(new String[] { "-file", "files/ec/simple-example.params" });
+		
+
+	}
+
+}
---
+++ evodta/src/main/java/com/aarribas/evodta/ecj/EvoDTAEvaluator.java
@@ -0,0 +1,68 @@
+package com.aarribas.evodta.ecj;
+
+import static java.util.Arrays.asList;
+
+import java.util.Collection;
+
+import org.jppf.task.storage.DataProvider;
+
+import rinde.ecj.DefaultResult;
+import rinde.ecj.GPBaseNode;
+import rinde.ecj.GPEvaluator;
+import rinde.ecj.GPProgram;
+import rinde.ecj.GPProgramParser;
+import com.aarribas.evodta.ecj.EvoDTAEvaluator.ExampleContext;
+import com.aarribas.evodta.ecj.EvoDTAEvaluator.ExampleTask;
+import rinde.jppf.ComputationTask;
+import ec.EvolutionState;
+import ec.gp.GPTree;
+
+
+public class EvoDTAEvaluator extends GPEvaluator<ExampleTask, DefaultResult, GPProgram<ExampleContext>> {
+
+	@Override
+	protected Collection<ExampleTask> createComputationJobs(DataProvider dataProvider, GPTree[] trees,
+			EvolutionState state) {
+		final GPProgram<ExampleContext> prog = GPProgramParser
+				.convertToGPProgram((GPBaseNode<ExampleContext>) trees[0].child);
+		return asList(new ExampleTask(prog));
+	}
+
+	@Override
+	protected int expectedNumberOfResultsPerGPIndividual(EvolutionState state) {
+		return 1;
+	}
+
+	public static class ExampleTask extends ComputationTask<DefaultResult, GPProgram<ExampleContext>> {
+
+		public ExampleTask(GPProgram<ExampleContext> p) {
+			super(p);
+		}
+
+		public void run() {
+			double diff = 0;
+			for (int x = 0; x < 10; x++) {
+				for (int y = 0; y < 10; y++) {
+					final double goal = (x * x) + y;
+					final double result = taskData.compute(new ExampleContext(x, y));
+					diff += Math.abs(goal - result);
+				}
+			}
+			float fitness = (float) diff;
+			if (Float.isInfinite(fitness) || Float.isNaN(fitness)) {
+				fitness = Float.MAX_VALUE;
+			}
+			setResult(new DefaultResult(fitness, taskData.getId()));
+		}
+	}
+
+	public static class ExampleContext {
+		public final int x;
+		public final int y;
+
+		public ExampleContext(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
